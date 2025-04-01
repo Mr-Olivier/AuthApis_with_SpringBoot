e5a78dbb78d88addc13641b98595d1ce45e7a3a2
Olivier <iradukundaolivier2000@gmail.com>
2025-04-01 21:46:07 +0200

    Add User entity with OTP verification

---
+++ src/main/java/com/auth/authservice/entity/User.java
@@ -0,0 +1,62 @@
+package com.auth.authservice.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean isVerified = false;
+
+    @Column
+    private String otp;
+
+    @Column
+    private LocalDateTime otpGeneratedTime;
+
+    @Column
+    private LocalDateTime otpVerifiedTime;
+
+    @Column
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
